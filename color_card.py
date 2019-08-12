1e39697b7e74f3ab3de72d86c110c409848b6170
chdu <duchenpaul@gmail.com>
2019-08-12 10:23:44 +0800

    Fix the error when the confidence is 1

--- color_card.py
+++ color_card.py
@@ -5,7 +5,7 @@ green = Color("#2ECC71")
 yellow = Color("#F1C40F")
 
 def color_card(num):
-    colors = [ x.hex_l for x in list(red.range_to(yellow,50)) + list(yellow.range_to(green,50))]
+    colors = [ x.hex_l for x in list(red.range_to(yellow,50)) + list(yellow.range_to(green,51))]
     return colors[int(num)]
 
 if __name__ == '__main__':
