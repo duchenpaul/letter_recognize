86d4cf61ee2153c98b8d8696259ec0af4fa58bc9
duchenpaul <duchenpaul@gmail.com>
2019-10-02 18:42:27 +0800

    Update train_keras.py

--- train_keras.py
+++ train_keras.py
@@ -39,20 +39,20 @@ MODELNAME_FILE = MODELNAME + '.model'
 MODELNAME_FULL_PATH = os.path.join(model_dir, MODELNAME_FILE)
 
 
-def gen_meta(X_dataset, Y_dataset):
+def gen_meta(X_test_dataset, Y_test_dataset):
     from PIL import Image
     sample_size = 100
-    # print(X_dataset.shape)
-    X_dataset_sample = X_dataset[:sample_size*sample_size, :, :, :]
-    Y_dataset_sample = Y_dataset[:sample_size*sample_size]
-    # print(X_dataset_sample.shape)
-    img_array = X_dataset_sample.reshape(sample_size, sample_size, IMG_SIZE, IMG_SIZE)
+    # print(X_test_dataset.shape)
+    X_test_dataset_sample = X_test_dataset[:sample_size*sample_size, :, :, :]
+    Y_test_dataset_sample = Y_test_dataset[:sample_size*sample_size]
+    # print(X_test_dataset_sample.shape)
+    img_array = X_test_dataset_sample.reshape(sample_size, sample_size, IMG_SIZE, IMG_SIZE)
     img_array_flat = np.concatenate([np.concatenate([x for x in row], axis=1) for row in img_array])
     img = Image.fromarray(np.uint8(255 * (1. - img_array_flat)))
     log_dir = os.path.join(LOG_DIR, MODELNAME)
     os.mkdir(log_dir)
     img.save(os.path.join(log_dir, 'images.jpg'))
-    np.savetxt(os.path.join(log_dir, 'metadata.tsv'), np.where(Y_dataset_sample)[1], fmt='%d')
+    np.savetxt(os.path.join(log_dir, 'metadata.tsv'), np.where(Y_test_dataset_sample)[1], fmt='%d')
 
 
 def data_preprocess():
@@ -106,7 +106,7 @@ def train_model(model, X_dataset, Y_dataset):
                              write_grads=True,  # 是否可视化梯度直方图
                              write_images=True,  # 是否可视化参数
                              embeddings_freq=10,
-                             embeddings_data = X_dataset[:100],
+                             embeddings_data = X_dataset[:400],
                              embeddings_layer_names=embedding_layer_names,
                              embeddings_metadata=None
                              )
